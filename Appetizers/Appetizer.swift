e90c095704375f88048385b889b3666c19317f3e
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-04 14:20:17 +0200

    Add Appetizer model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,25 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Ahmed Nafie on 04/01/2025.
+//
+
+import Foundation
+
+// MARK: - AppetizerResponse
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+// MARK: - Appetizer
+struct Appetizer: Decodable {
+    let protein: Int
+    let calories: Int
+    let price: Double
+    let name: String
+    let carbs: Int
+    let imageURL: String
+    let description: String
+    let id: Int
+}
