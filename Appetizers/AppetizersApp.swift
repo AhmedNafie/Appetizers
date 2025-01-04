c81e2542ff674f948e7dc45c5dc76d06beddde54
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-04 12:43:07 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ahmed Nafie on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
