74e90a827aa42a4c136306b02930712a9756570f
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-04 13:27:50 +0200

    Add TabView

--- Appetizers/AppetizersTabView.swift
+++ Appetizers/AppetizersTabView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct AppetizersTabView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            AppetizersListScreen()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            AccountScreen()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            OrderScreen()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
         }
-        .padding()
     }
 }
 
