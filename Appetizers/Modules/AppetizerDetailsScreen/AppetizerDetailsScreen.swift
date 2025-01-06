3d5a7658c744a79967905688ad7dd07fa941d41b
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-06 20:00:06 +0200

    Add AppetizerDetailsScreen

---
+++ Appetizers/Modules/AppetizerDetailsScreen/AppetizerDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerDetailsScreen.swift
+//  Appetizers
+//
+//  Created by Ahmed Nafie on 06/01/2025.
+//
+
+import SwiftUI
+
+struct AppetizerDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailsScreen()
+}
