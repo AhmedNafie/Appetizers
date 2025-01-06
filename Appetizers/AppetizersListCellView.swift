f1b7338732841fef8c3d61a9d9ac83c58e858772
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-06 16:32:36 +0200

    Populate AppetizersList with remote images

--- Appetizers/AppetizersListCellView.swift
+++ Appetizers/AppetizersListCellView.swift
@@ -11,9 +11,7 @@ struct AppetizersListCellView: View {
     let appetizer: Appetizer
     var body: some View {
         HStack {
-            Image("foodPlaceHolder")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            AppetizerRemoteImage(urlString: appetizer.imageURL)                .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
             
