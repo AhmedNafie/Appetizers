29b8fadeae54f99406ba16e237853f5d9c271d1f
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-06 16:30:13 +0200

    Handle image network Call

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -5,10 +5,11 @@
 //  Created by Ahmed Nafie on 04/01/2025.
 //
 
-import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     static let appetizersEndpoint = "appetizers"
     private let appetizersURL = baseURL + appetizersEndpoint
@@ -41,6 +42,27 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        task.resume()
+    }
+
+    func downloadImage(fromURLString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: fromURLString)
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        guard let url = URL(string: fromURLString) else {
+            completed(nil)
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         task.resume()
     }
