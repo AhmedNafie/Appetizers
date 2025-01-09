3a0f38ab77e72d2ef27c0821378a2dcc95c29e1e
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2025-01-09 21:46:52 +0200

    Add list removing functionality

--- Appetizers/Modules/Order/OrderScreen.swift
+++ Appetizers/Modules/Order/OrderScreen.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct OrderScreen: View {
+    @State private var orderItems = Appetizer.orderAppetizers
     var body: some View {
         NavigationView {
             VStack {
@@ -15,6 +16,7 @@ struct OrderScreen: View {
                     ForEach(Appetizer.orderAppetizers) { appetizer in
                         AppetizersListCellView(appetizer: appetizer)
                     }
+                    .onDelete(perform: deleteItems)
                 }
                 .listStyle(PlainListStyle())
                 
@@ -22,6 +24,10 @@ struct OrderScreen: View {
             }
                 .navigationTitle("🗒️ Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
